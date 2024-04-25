5e43c27c78266fdb2fd2a8ea67e2645869363736
katrinnn19 <lgfox941@gmail.com>
2024-04-25 16:10:02 +0200

    Velocity_gorod_tvoey_mechtu
    P.S. Net, shutka

---
+++ Assets/Scripts/AiShell.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AiShell : MonoBehaviour
+{
+    public GameObject explosion;
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "tank")
+        {
+            GameObject exp = Instantiate(explosion, this.transform.position, Quaternion.identity);
+            Destroy(exp, 0.5f);
+            Destroy(this.gameObject);
+        }
+    }
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
